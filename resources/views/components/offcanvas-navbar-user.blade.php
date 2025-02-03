9662bb930846283b3fa12e51fc228cc5a36cddf6
AdrianoMiguell <adrianosantos260804@gmail.com>
2025-02-02 21:39:21 -0300

    Pequenos ajustes no navbar

--- resources/views/components/offcanvas-navbar-user.blade.php
+++ resources/views/components/offcanvas-navbar-user.blade.php
@@ -3,10 +3,10 @@
     <i class="bi bi-list"></i>
 </button>
 
-<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvas-navbar-user"
-    aria-labelledby="offcanvas-navbar-userLabel" style="width: 300px;">
+<div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1"
+    id="offcanvas-navbar-user" aria-labelledby="offcanvas-navbar-userLabel" style="width: 300px;">
     <div class="offcanvas-header">
-        <h5 class="offcanvas-title d-inline-block text-truncate" id="offcanvas-navbar-userLabel"
+        <h5 class="offcanvas-title d-inline-block text-truncate p-3" id="offcanvas-navbar-userLabel"
             style="max-width: 250px; overflow: hidden;">
             @auth
                 <a href="{{ route('profile.view') }}">
@@ -14,7 +14,7 @@
                 </a>
             @endauth
             @guest
-                Convidado
+                Cursos Play
             @endguest
         </h5>
         <button type="button" class="offcanvas-btn-close" data-bs-dismiss="offcanvas" aria-label="Close">
@@ -22,22 +22,60 @@
         </button>
     </div>
     <div class="offcanvas-body">
-        <h2> Convidado </h2>
-        <p class="m-2 mx-4">
-            Opções de controle. Todos as alterações devem ser feitas com cuidado, pois
-            algumas podem ser
-            inalteráveis.
-        </p>
-
-        <div class="my-5">
-            <ul class="offcanvas-menu">
-                <li class="offcanvas-item"><span> lorem y 1</span></li>
-                <li class="offcanvas-item"><span> lorem y 2</span></li>
-                <li class="offcanvas-item"><span> lorem y 3</span></li>
-                <li class="offcanvas-item"><span> lorem y 4</span></li>
+        @if (Auth::check() && Auth::user()->type == 1)
+            <ul class="p-4 offcanvas-menu">
+                <li class="offcanvas-item">
+                    <a href="{{ route('workspace') }}">
+                        <i class="bi bi-eye-fill"></i>
+                        <span>Cursos</span>
+                    </a>
+                </li>
+                <li class="offcanvas-item">
+                    <a href="">
+                        <i class="bi bi-plus"></i>
+                        <span>Curso</span>
+                    </a>
+                </li>
+                <li class="offcanvas-item">
+                    <a href="">
+                        <i class="bi bi-patch-check"></i>
+                        <span>Certificados</span>
+                    </a>
+                </li>
+                <li class="offcanvas-item">
+                    <a href="">
+                        <i class="bi bi-person-workspace"></i>
+                        <span>Matriculas</span>
+                    </a>
+                </li>
+                <li class="offcanvas-item">
+                    <a href="">
+                        <i class="bi bi-envelope-check-fill"></i>
+                        <span>Termos</span>
+                    </a>
+                </li>
             </ul>
+        @else
+            <div class="search my-3 p-2">
+                <form action="{{ route('user.view_cursos') }}" method="get" class="d-flex form-search">
+                    <input type="search" name="search" id="search" class="form-control"
+                        value="{{ isset($search) ? $search : '' }}">
+                    <button type="submit">
+                        <i class="bi bi-search"></i>
+                    </button>
+                </form>
+            </div>
 
-        </div>
-
+            <ul class="navbar-nav">
+                @if (!isset(Auth::user()->id))
+                    <li class="offcanvas-item">
+                        <a href="{{ route('register') }}" class="ml-4 btnGeral">Register</a>
+                    </li>
+                    <li class="offcanvas-item">
+                        <a href="{{ route('login') }}" class="btnGeral">Log in</a>
+                    </li>
+                @endif
+            </ul>
+        @endif
     </div>
 </div>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,7 +1,6 @@
 <nav class="navbar">
-    <a href="{{ isset(Auth::user()->id) ? route('dashboard') : '\\'  }}" class="nav-brand">
-        <img src="https://lh3.googleusercontent.com/Do4YWzwlk61PqRSKr_duNOUn-ivFAE8ErrcdrXCx5_8leA0LkDwz7cjxnlXEIwez-pgP12aJMXqzeEKJzrNZw5vxi9MvM2hlBPpj3aOsqXtdVKDeclE6ybzM2P8HYMkLULS7UrwH_u9t_8hkZzCzXIf6NEHaV2V0zKc_tNDO9FKpd264EtEA9021GcmK3vlJKvmCDlm8sCzaLvFG6JAGBF_WqMjHOVHePy7AByiLfyUZG40lJVwu55gNWqg0gxdzsuvGbUq7a0OnNO11SDIPwy4JqYRlAQLFMAjdxFIIUTB1fLrd_NQUb9uhEKcoUu6vE2ITVYFpD6riiPGjEJWV8v9xubEIUuxJ6h_iNMm8GD0mXJqBzQd76iT5oc6elJSfQ1qlLTWreHcdCghbfuaXD0iS2X3x2lBxYRIXC8DW5mPtupvn-yA2nEyq3MSSMWvdwFko1gLbexfmo-DbNyxr1iYPGHL0Rje5WfkEK28_nB9OqcbQZeKnPt5WVTEzedCZi1yt0urawbLqqh4RHNoeQRoRDkrKu9ZHKuMD0Kb3zMivLHphpRaz_RH6-GghJtEmsmasIariA_-pjEhzlnR-_-Wq9C3-ptKx8caoJTmJwzeokQ5hbrgNtBB5pc2RXmow5dvhnU4v3PdDU2-4ZQKEXf295WLnh36cof_20zbMCcj2HdIJ3yIU1dc2UdECOccNMfJ8xG-tl0TW1gxbD_yy3ENbrcW3mn3XfYIZSWcWojJ155z0GdHzLhj81LoVylElW9gFo_Xqwsbtmz-LYGUge1WNGW8JtSKvBjU4rjOUSBfLoJEoribVi2P1IXpLjImDhbrupe327cRmuEFEVhMAz92FX-BFR5TNVOsuEmZ1yr9u1j1moWG5_e6Vyu7uWwEVvabI0-t1UdfQlnPjAk3XNf3Am4nZKH7RVHJ5E-__jbwxuO9beoTfC3mUygQIR_-ZGmcW4cmE_coHAx3MtNs=w500-h380-s-no?authuser=0"
-            alt="logo do site cursos play" class="brand">
+    <a href="{{ isset(Auth::user()->id) ? route('dashboard') : '\\' }}" class="nav-brand">
+        <img src="./img/icons/logo_curso.png" alt="logo do site cursos play" class="brand">
         <span>Cursos Play</span>
     </a>
 
@@ -73,14 +72,13 @@
                     </li>
                 @endauth
                 @guest
-                    <li class="nav-item nav-item-control">
+                    <li class="nav-item">
                         <a href="{{ route('register') }}" class="ml-4 btnGeral">Register</a>
                     </li>
                     <li class="nav-item nav-item-control">
                         <a href="{{ route('login') }}" class="btnGeral">Log in</a>
                     </li>
                 @endguest
-
                 <li class="nav-item nav-item-offcanvas">
                     @include('components.offcanvas-navbar-user')
                 </li>
